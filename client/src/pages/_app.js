b012b532cffe614bc1509b9d4ac77320cde50db4
thatanjan <thatanjan@gmail.com>
2021-12-15 19:26:50 +0600

    client is setup with mui

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import * as React from 'react'
+import Head from 'next/head'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import createEmotionCache from 'utils/createEmotionCache'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+
+export default function MyApp(props) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+	return (
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<title>My page</title>
+				<meta name='viewport' content='initial-scale=1, width=device-width' />
+			</Head>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</CacheProvider>
+	)
 }
-
-export default MyApp
